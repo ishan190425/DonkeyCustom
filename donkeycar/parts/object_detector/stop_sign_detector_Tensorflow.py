e0204277a5ced3d8a4759765dc040579003b2cc1
Ishan R <rathiishan0@gmail.com>
2023-01-26 12:33:01 -0500

    Changed config load

--- donkeycar/parts/object_detector/stop_sign_detector_Tensorflow.py
+++ donkeycar/parts/object_detector/stop_sign_detector_Tensorflow.py
@@ -20,7 +20,6 @@ class StopSignDetector(object):
     
     def __init__(self, show_bounding_box, max_reverse_count=0, reverse_throttle=-0.5, debug=False):
         self.cfg = dk.load_config(config_path='/home/pi/mycar/myconfig.py')
-        args = docopt(__doc__)
         MODEL_NAME = self.cfg.STOP_SIGN_MODEL
         PATH_TO_MODEL_DIR = self.download_model(MODEL_NAME)
         PATH_TO_SAVED_MODEL = PATH_TO_MODEL_DIR + "/saved_model"
