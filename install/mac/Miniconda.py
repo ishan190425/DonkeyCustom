24b24037fab06ee975123ae0c4bd311b63417736
Ishan R <rathiishan0@gmail.com>
2023-02-01 14:03:40 -0500

    Started script to autodownload

---
+++ install/mac/Miniconda.py
@@ -0,0 +1,13 @@
+import sys
+import subprocess
+import urllib.request
+
+class Miniconda():
+    def __init__(self) -> None:
+        pass
+    
+    def install(self) -> int:
+        tup = urllib.request.urlretrieve("https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh","install/mac/miniconda.sh")
+        if tup[0] is None:
+            return -1
+        return 0
